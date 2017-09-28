e0a2c86d3791a193d21eb950d9a141fe6d0d648a
Tuscann <fbinnzhivko@gmail.com>
2017-09-28 23:09:52 +0300

    Add YouTube Links

--- 08.Exercise String Processing/src/test.java
+++ 08.Exercise String Processing/src/test.java
@@ -1,10 +1,29 @@
 package src;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class test {
-    public static void main(String[] args) throws IOException
+    static int i = 0;
+    public static void main(String[] args) throws IOException {
+
+        int[] a = IntStream.range(1, 100).toArray();
+
+
+
+            try {
+                recurse();
+            } catch (Throwable t) {
+                System.exit(0);
+            }
+
+
+
+    }
+    private static void recurse()
     {
+        System.out.print(++i + 0 / (i - 100) + " ");
+        recurse();
 
     }
 }

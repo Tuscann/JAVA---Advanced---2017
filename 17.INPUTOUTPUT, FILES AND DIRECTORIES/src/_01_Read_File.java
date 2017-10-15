cc3b8a49ba65812733adf90918626e4b35ad2cae
Tuscann <fbinnzhivko@gmail.com>
2017-10-15 20:42:50 +0300

    add solutions of files

--- 17.INPUTOUTPUT, FILES AND DIRECTORIES/src/_01_Read_File.java
+++ 17.INPUTOUTPUT, FILES AND DIRECTORIES/src/_01_Read_File.java
@@ -1,2 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class _01_Read_File {
+    public static void main(String[] args) {
+        String inputPath = "C:\\Users\\123\\Documents\\Java---Advanced---2017\\17.INPUTOUTPUT, FILES AND DIRECTORIES\\src\\input.txt";
+
+        try (FileInputStream fis = new FileInputStream(inputPath)) {
+            int oneByte = fis.read();
+
+            while (oneByte >= 0) {
+                System.out.print(Integer.toBinaryString(oneByte) + " ");
+                oneByte = fis.read();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- 17.INPUTOUTPUT, FILES AND DIRECTORIES/src/_02_Write_to_a_File.java
+++ 17.INPUTOUTPUT, FILES AND DIRECTORIES/src/_02_Write_to_a_File.java
@@ -1,2 +1,32 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class _02_Write_to_a_File {
+    public static void main(String[] args) {
+        String inputPath = "C:\\Users\\123\\Documents\\Java---Advanced---2017\\17.INPUTOUTPUT, FILES AND DIRECTORIES\\src\\input2.txt";
+        String outputPath = "C:\\Users\\123\\Documents\\Java---Advanced---2017\\17.INPUTOUTPUT, FILES AND DIRECTORIES\\src\\output.txt";
+        Set<Character> punctuation = new HashSet<>();
+        Collections.addAll(punctuation, '.', ',', '!', '?');
+
+        try (FileInputStream fis = new FileInputStream(inputPath);
+             FileOutputStream fos = new FileOutputStream(outputPath)) {
+
+            int oneByte = fis.read();
+
+            while (oneByte >= 0) {
+                if (!punctuation.contains((char) oneByte)) {
+                    fos.write(oneByte);
+                }
+
+                oneByte = fis.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
